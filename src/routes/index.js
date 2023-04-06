fa40abb62113ded1552fdcbe4eb72b69da60f831
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-04-06 09:45:38 -0300

    feat: tab bar styling

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,48 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../pages/home';
 import Favorites from '../pages/favorites';
 
+import { Ionicons } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
 export function Routes() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Favorites" component={Favorites} />
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarHideOnKeyboard: true,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#121212',
+            tabBarStyle: {
+                backgroundColor: '#fff',
+                borderTopWidth: 0,
+            }
+          }}
+        >
+            <Tab.Screen 
+                name="Home" 
+                component={Home} 
+                options={{
+                    tabBarIcon: ({color, size, focused}) => {
+                        if (focused) {
+                            return <Ionicons name="home" size={size} color='#333' />
+                        }
+                        return <Ionicons name="home-outline" size={size} color={color} />
+                    }
+                }}
+            />
+            <Tab.Screen 
+                name="Favorites" 
+                component={Favorites} 
+                options={{
+                    tabBarIcon: ({color, size, focused}) => {
+                        if (focused) {
+                            return <Ionicons name="heart" size={size} color='#ff4141' />
+                        }
+                        return <Ionicons name="heart-outline" size={size} color={color} />
+                    }
+                }}
+            />
         </Tab.Navigator>
     );
 }
